11898bea69df255d3022912c28d13211233318ed
jdiouris <32338545+jdiouris@users.noreply.github.com>
2019-11-15 08:34:41 +0100

    Add files via upload

---
+++ accesspointcon.h
@@ -0,0 +1,180 @@
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   7  // MUST be an interrupt pin!
+
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
+
+#define WLAN_SSID       "etn-scm-iot"
+#define WLAN_PASS       "etn-scm-iot-112"
+
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+
+
+
+
+/**************************************************************************/
+/*!
+    @brief  Displays the driver mode (tiny of normal), and the buffer
+            size if tiny mode is not being used
+
+    @note   The buffer size and driver mode are defined in cc3000_common.h
+*/
+/**************************************************************************/
+void displayDriverMode(void)
+{
+  #ifdef CC3000_TINY_DRIVER
+    Serial.println(F("CC3000 is configure in 'Tiny' mode"));
+  #else
+    Serial.print(F("RX Buffer : "));
+    Serial.print(CC3000_RX_BUFFER_SIZE);
+    Serial.println(F(" bytes"));
+    Serial.print(F("TX Buffer : "));
+    Serial.print(CC3000_TX_BUFFER_SIZE);
+    Serial.println(F(" bytes"));
+  #endif
+}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the CC3000's internal firmware patch ID
+*/
+/**************************************************************************/
+uint16_t checkFirmwareVersion(void)
+{
+  uint8_t major, minor;
+  uint16_t version;
+  
+#ifndef CC3000_TINY_DRIVER  
+  if(!cc3000.getFirmwareVersion(&major, &minor))
+  {
+    Serial.println(F("Unable to retrieve the firmware version!\r\n"));
+    version = 0;
+  }
+  else
+  {
+    Serial.print(F("Firmware V. : "));
+    Serial.print(major); Serial.print(F(".")); Serial.println(minor);
+    version = major; version <<= 8; version |= minor;
+  }
+#endif
+  return version;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the 6-byte MAC address of the CC3000 module
+*/
+/**************************************************************************/
+void displayMACAddress(void)
+{
+  uint8_t macAddress[6];
+  
+  if(!cc3000.getMacAddress(macAddress))
+  {
+    Serial.println(F("Unable to retrieve MAC Address!\r\n"));
+  }
+  else
+  {
+    Serial.print(F("MAC Address : "));
+    cc3000.printHex((byte*)&macAddress, 6);
+  }
+}
+
+bool getGatewayIp(uint32_t *gateway)
+{
+  uint32_t ipAddress, netmask, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.println();
+    return true;
+  }
+  
+}
+
+ 
+/**************************************************************************/
+/*!
+    @brief  Tries to read the IP address and other connection details
+*/
+/**************************************************************************/
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    //ipBroker=gateway;
+    Serial.println();
+    return true;
+  }
+  
+}
+
+void connectAP()
+{
+   displayDriverMode();
+  
+  Serial.println(F("\nInitialising the CC3000 ..."));
+  if (!cc3000.begin()) {
+    Serial.println(F("Unable to initialise the CC3000! Check your wiring?"));
+    for(;;);
+  }
+
+
+  
+  displayMACAddress();
+  
+  Serial.println(F("\nDeleting old connection profiles"));
+  if (!cc3000.deleteProfiles()) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+
+  /* Attempt to connect to an access point */
+  char *ssid = WLAN_SSID;             /* Max 32 chars */
+  Serial.print(F("\nAttempting to connect to ")); Serial.println(ssid);
+  
+  /* NOTE: Secure connections are not available in 'Tiny' mode! */
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+   
+  Serial.println(F("Connected!"));
+  
+  /* Wait for DHCP to complete */
+  Serial.println(F("Request DHCP"));
+  while (!cc3000.checkDHCP()) {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }
+
+  /* Display the IP address DNS, Gateway, etc. */  
+  while (!displayConnectionDetails()) {
+    delay(1000);
+  }
+}
+
+
